8fe89e59989bf7ff89372a86edcc90581186c5d0
gauravjangra116 <gaurav2206.be21@chitkara.edu.in>
2025-02-12 15:37:20 +0530

    Lec6 Update

---
+++ Lec6_120225/index.js
@@ -0,0 +1,63 @@
+// console.log(1);
+// console.log(SumofTwo(10,5));
+// function SumofTwo(a,b){
+//     return a+b;
+// }
+// setTimeout(()=>{
+//     console.log(4)
+// },2000);
+// var start =1;
+// var end = 1000000000000;
+// while(start<end){
+//     start++;
+// }
+
+// console.log("End")
+// setInterval(() => {
+//     console.log(9)
+// }, 4000);
+
+
+//Types of Functions
+
+//Function statement
+// function xyz(){
+//     console.log('zy');
+//   }
+  
+//   xyz(); //FUcntion Calling
+
+
+  
+//   var x = function(){
+//     console.log("Annonymus Function")
+//   }
+  
+//   console.log(x())
+
+
+
+  
+//   function sum(x,y){
+//     return x+y
+//   }
+//   var total = sum(10,5);
+//   console.log(total);
+  
+  
+  
+//   //Arrow Function
+//   const add = (x, y) => x + y;
+//   console.log(add(5, 3));
+
+
+function xyz(){
+    console.log(1);
+}
+
+var abc = function cdf(){
+    console.log(2);
+}
+
+xyz();
+abc();
